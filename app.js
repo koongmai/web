b882f55ec6667d8e4e571c01b99caf09b98a01a0
kyungmin <minii7582@konkuk.ac.kr>
2025-06-05 12:58:16 +0900

    hihi

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
-// express 기본 모듈 불러오기
-var express =require('express'),http=require("http"),path =require('path');
-// express 미들웨어 불러오기
-var static = require('serve-static');
-// express 객체 생성
-var app = express();
-var router = express.Router();
+// Express는 외장 모듈로, npm install express로 설치해야 함
+// serve-static은 정적 파일을 제공하기 위한 외장 미들웨어로, 특정 폴더의 파일을 특정 경로로 노출할 때 사용
+let express = require('express');
+let static = require('serve-static'); 
 
-//기본 속성 설정
-app.set('port', process.env.PORT||8080); //static ip를 공유해서 쓴다? , 사설ip는 외부에서는 안 보이지만,,
-app.set('host', '127.0.0.1'); //호스트에는 루프백..
+//node.js 내장 모듈, http, path, https, fs 이고, 호출은 require() 사용용
+let http = require('http'); 
+let path = require('path'); 
+const https = require('https');
+const fs = require('fs');
 
-//static 서버 미들웨어 사용
-app.use(static(__dirname)); //현재 폴더에 대한 정적 접근 허용
+const options = {
+   key: fs.readFileSync("cert.key"),
+   cert: fs.readFileSync("cert.crt")
+};
+//express 객체 생성 app객체랑 router객체 
+let app = express(); 
+let router = express.Router(); 
 
-app.get('/', function(req, res) {
-    res.redirect('/source/jquery.html');
-});
+//app.set (): 설정값을 저장하는 메서드드
+app.set('port', process.env.PORT || 8080); //하나의 컴퓨터에서 어떤 서비스에 연결할 건지
+app.set('host', '192.168.38.56'); //어디에 있는 컴퓨터인가? 컴퓨터 ip or localhost
 
-app.get('/routetest', function(req, res) {
-    res.redirect('http://www.google.com');
-});
-
-
-app.use(function(req,res,next){
-	console.log('첫 번째 미들웨어에서 요청을 처리함.');
-	req.user = 'mike';
-	next();
-})
-
-app.use('/',function(req,res,next){
-	console.log('두번 째 미들웨어에서 요청을 처리함.');
-	res.writeHead('200',{'Content-Type' : 'text/html;charset=utf8'});
-	res.end('<h1>Express 서버에서 ' + req.user + '가 응답한 결과입니다.</h1>');
-})
-
+app.use(static(__dirname));
 app.use(express.urlencoded());
 app.use(express.json());
-//http 서버 생성
-http.createServer(app).listen(app.get('port'),app.get('host'),()=>{
-	console.log('Express server running at'+app.get('port')+app.get('host'));
+
+router.route('/').get(function(req,res){
+   res.redirect('/source/jquery.html');
+});
+
+router.route('/routetest').get(function(req, res){
+   res.redirect('http://www.google.com');
+});
+
+router.route('/rss').get(function(req, res){
+   console.log("rss data requested");
+   let feed = "https://news.sbs.co.kr/news/headlineRssFeed.do?plink=RSSREADER";
+   https.get(feed, function(httpres){
+      let rss_res = "";
+      httpres.on('data',function(chunk){
+         rss_res += chunk;
+      });
+      httpres.on('end',function(){
+         res.send(rss_res);
+         console.log("rss response completed");
+         res.end();
+      });
+   });
+});
+
+app.use('/',router);
+
+http.createServer(app).listen(app.get('port'), app.get('host'), ()=>{
+   console.log('Express server running at ' + app.get('port')+app.get('host'));
+});
+
+const PORT = 8000;
+https.createServer(options,app).listen(PORT, app.get('host'), ()=>{
+console.log('Express HTTPS server running at https://' + app.get('host') + ':' + PORT);
 });
--- source/jquery.js
+++ source/jquery.js
@@ -1,260 +1,295 @@
-var i = 0;
+$(document).ready(function() { 
+//html문서가 다 로드된 뒤에 실행하라. 여기에 모든 jquery 코드(실행 시점에 필요한 코드)작성!
+    var i=0; //전역변수, 여러 이벤트 함수에서 공유 가능
+    $("div.out").mouseover(function(){
+        $("div.out p:first").text("mouse over"); //.text("ㅇ");해당 요소 안의 텍스트가 바뀜. 
+        $("div.out p:last").text(++i); 
+        //만약 var i가 이 함수 내부에 선언되어있다면, 
+        //이벤트함수가 호출될 때마다 i=0부터 다시 시작돼서 항상 1만 출력됨. 
+    });
 
-function handleClick() {
-    alert("Clicked!");
-}
+    $("div.out").mouseout(function(){
+        $("div.out p:first").text("mouse out");
+    });
 
-$(document).ready(function () {
-    // 초기 설정: 노트 입력 폼 숨기기
-    $("#note_form").hide();
-
-    // div.out 마우스 이벤트 처리
-    $("div.out")
-        .mouseover(function () {
-            $(this).find("p:first").text("mouse over");
-            $(this).find("p:last").text(++i);
-        })
-        .mouseout(function () {
-            $(this).find("p:first").text("mouse out");
-        });
-
-    // 버튼 클릭 시 새 창 열기
     $("#b1").on("click",
-        {
-            url: "http://www.google.com",
-            winattributes: "resize=1, scrollbars=1, status=1"
-        },
-        max_open
-    );
+        {url: "http://www.google.com",
+            winattributes: "resize=1, scrollbars=1, status=1"}, max_open);
 
-    function max_open(event) {
-        var maxwindow = window.open(event.data.url, "", event.data.winattributes);
-        maxwindow.moveTo(0, 0);
-        maxwindow.resizeTo(screen.availWidth, screen.availHeight);
-    }
-
-    // 버튼 클릭 생겼다가 지웠다가
-    $("#bind").click(function () {
-        $("#theone").on("click", handleClick);
+    $("#bind").click(function(){
+        $("body")
+        .on("click", "#theone", flash)
+        .find("#theone")
+        .text("Can Click!");
     });
 
-    $("#unbind").click(function () {
-        $("#theone").off("click", handleClick);
+    $("#unbind").click(function(){
+        $("body")
+        .off("click", "#theone", flash)
+        .find("#theone")
+        .text("Does nothing...");
     });
 
-    // 실습3 - 버튼 클릭 횟수
-    $("#trigger_test button:first").on("click", function () {
-        let count = parseInt($("#trigger_test span:first").text());
-        $("#trigger_test span:first").text(count + 1);
+    $("#trigger_test button:first").click(function(){
+        update($("#trigger_test span:first"));
     });
 
-    $("#trigger_test button:eq(1)").on("click", function () {
-        let count = parseInt($("#trigger_test span:eq(1)").text());
-        $("#trigger_test span:eq(1)").text(count + 1);
-
+    $("#trigger_test button:last").click(function(){
         $("#trigger_test button:first").trigger("click");
+        update($("#trigger_test span:last"));
     });
 
-    // 이미지 토글
-    $("#image").on("click", function () {
-        if ($(this).attr("data-state") === "1") {
-            $(this).attr("src", "karina2.jpg");
-            $(this).attr("data-state", "2");
-        } else {
-            $(this).attr("src", "karina.jpg");
-            $(this).attr("data-state", "1");
-        }
+    $("#image").click(function() {
+        let current = $(this).attr("src");
+        let next = (current.includes("karina.jpg")) ? "karina2.jpg" : "karina.jpg";
+        $(this).attr("src", next);
     });
 
-    // 사진 앨범 이미지 변경
     var imgArray = ["jae1.jpg", "jae2.jpg", "jae3.jpg", "jae4.jpg", "jae5.jpg"];
     var albumIndex = 0;
+
     $("#imgAlbum").attr("src", imgArray[albumIndex]);
-    $("#imgAlbum").click(function () {
+    $("#imgAlbum").click(function(){
         albumIndex = (albumIndex + 1) % imgArray.length;
         $("#imgAlbum").attr("src", imgArray[albumIndex]);
     });
 
-    // 메뉴 스타일 변경
-    $(".main-menu").mouseover(function () {
-        $(this).css({
-            "font-size": "20px",
-            "background-color": "green"
-        });
+    $(".main-menu").mouseover(function(){
+        var menu=$(this);
+        menu.css({"background-color":"green", "font-size":"20px"});
+    });
+    $(".main-menu").mouseout(function(){
+        var menu=$(this);
+        menu.css({"background":"none", "font-size":"1em"});
     });
 
-    $(".main-menu").mouseout(function () {
-        $(this).css({
-            "font-size": "",
-            "background-color": ""
-        });
-    });
 
-    // 플러스 이미지 클릭 시 note_form 보이기
-    $("#add_img").click(function () {
-        $("#note_form").addClass("popup").show();
-        $(this).hide();
-    });
+    // $("#add_img").click(function(){
+    //  $("#note_form").css({"display":"block"});
+    // });
 
-    // 확인 버튼 클릭 시 note에 내용 추가
-    $("#add_note").click(function () {
-        const title = $("#note_title").val();
-        const date = $("#note_date").val();
-        const content = $("#note_content").val();
-
-        const noteHtml = `
-            <div class="note_item" style="border:1px solid #ccc; margin:10px; padding:10px;">
-                <h3>${title}</h3>
-                <p><strong>${date}</strong></p>
-                <p>${content.replace(/\n/g, "<br>")}</p>
-            </div>
-        `;
-
-        $("#note").append(noteHtml);
-        $("#note_title").val("");
-        $("#note_date").val("");
-        $("#note_content").val("");
-        $("#note_form").fadeOut(1000, function() {
-            $("#add_img").show();
-        });
-    });
-
-    function show_note_form() {
-        $("#note_form").addClass("popup");
+    // $("#add_note").click(function(){
+    //  $("note").append($("#note_title").val());
+    // });
+    $("#add_img img").on("click", show_note_form);
+    $("#add_note").on("click", push_note);
+    $(window).resize(function(){
         change_position($(".popup"));
-        $("#note_form").fadeIn(5000);
-    }
+    });
 
-    function change_position(obj){
-        var l=($(window).width()-obj.width())/2;
-        var t=($(window).height()-obj.height())/2;
-        obj.css({top:t, left:l})
-    }
-
-    function centerNoteForm() {
-        const $popup = $("#note_form");
-
-        if ($popup.is(":visible")) {
-            const winWidth = $(window).width();
-            const winHeight = $(window).height();
-            const popupWidth = $popup.outerWidth();
-            const popupHeight = $popup.outerHeight();
-
-            const left = (winWidth - popupWidth) / 2;
-            const top = (winHeight - popupHeight) / 2;
-
-            $popup.css({ top: top + "px", left: left + "px" });
-        }
-    }
-
-    $(window).on("resize", centerNoteForm);  // 창 크기 바뀔 때 위치 재조정
-
-    $("#moving_button").click(function() {
-        if($("#moving_box").width() <= $("#animation_test").width() - 30) {
-            $("#moving_box").animate({
-                right: '0px',
-                height: '+=50px',
-                width: '+=50px'
-            });
-
-            $("#animation_test").animate({
-                height: '+=50px'
-            });
+    $("#moving_button").click(function(){
+        if($("#animation_test").width()-50 > $("#moving_box").width()){
+            $("#moving_box").animate({right:'0px', width:'+=50px', height:'+=50px'});           
+            $("#animation_test").animate({height:'+=50px'});
         }
     });
-    $(".accordion").each(function () {
-        const dl = $(this);
-        const allDt = dl.find("dt");
-        const allDd = dl.find("dd");
 
-        function closeAll() {
+    // $(".accordion").each(function(){
+    //  var dl=$(this);
+    //  var alldd = dl.find("dd");
+    //  var alldt = dl.find("dt");
+    //  alldd.hide();
+    //  alldt.css("cursor","pointer");
+    //  alldt.click(function(){
+    //      alldd.hide();
+    //      var dt = $(this);
+    //      var dd = dt.next();
+    //      dd.show();
+    //      alldt.css("cursor","pointer");
+    //      dt.css("cursor", "default");
+    //  });
+    // });
+
+    $(".accordion").each(function (){
+        var dl = $(this);
+        var allDt = dl.find('dt');
+        var allDd = dl.find("dd");
+        function closeAll(){
             allDd.addClass("closed");
             allDt.addClass("closed");
         }
-
-        function open(dt, dd) {
+        function open(dt,dd){
             dt.removeClass("closed");
             dd.removeClass("closed");
         }
-
         closeAll();
-
-        allDt.on("click", function () {
-            const dt = $(this);
-            const dd = dt.next("dd");
+        allDt.click(function(){
+            var dt = $(this);
+            var dd = dt.next();
             closeAll();
             open(dt, dd);
-        });
+        })
     });
 
-    // 슬라이드쇼 기능
-    $(".slideshow").each(function () {
-        const container = $(this);
-        const imgs = container.find("img").hide().eq(0).show();
-        const interval = 3000;
-        let timer;
+    // $("dt").addClass("closed");
+    // $("dd").addClass("closed");
 
-        function switchImg() {
-            const first = container.find("img").eq(0);
-            const second = container.find("img").eq(1);
-
-            first.fadeOut(1000, function () {
-                first.appendTo(container);
-            });
-            second.fadeIn(1000);
+    var interval=4000;
+    $('.slideshow').each(function(){
+        var timer;
+        var container = $(this);
+        function switchImg(){
+            var imgs = container.find("img");
+            var first = imgs.eq(0);
+            var second = imgs.eq(1);
+            // container.append(first);
+            // first.fadeOut("slow");
+            first.appendTo(container).fadeOut(2000);
+            second.fadeIn();
         }
-
-        function startTimer() {
+        // setInterval(switchImg, interval);
+        function startTimer(){
             timer = setInterval(switchImg, interval);
         }
-
-        function stopTimer() {
+        function stopTimer(){
             clearInterval(timer);
         }
-
         container.hover(stopTimer, startTimer);
         startTimer();
     });
-});
 
-$(document).ready(function(){
     $("#getText1").on("click", function(){
         $("#textbox").text("글자 입력 테스트");
-        var req =$.ajax("data.json");
+        //txt, json
+        // var req = $.ajax("data.json"); //data.txt
+
+        //다른 방법
+        var req = $.ajax({
+            url: "data.txt",
+            dataType: "json"
+        });
+
         req.done(function(data, status){
-            var tb =$("<table/>");
+            //txt
+            // var students = JSON.parse(data);
+            // for(var i=0; i<students.length; i++){
+            //  var str = "<br>"+students[i].name;
+            //  $("#textbox").append(str);
+            // }
+
+            //json, 다른 방법
+            for(var i=0; i<data.length; i++){
+                var str = "<br>"+data[i].name;
+                $("#textbox").append(str);
+            }
+        });
+    });
+
+    $("#getText2").on("click", function(){
+        var req = $.ajax({
+            url: "data.txt",
+            dataType: "json"
+        });
+
+
+        req.done(function(data, status){    
+            
+            var tb = $("<table/>");
             var row = $("<tr/>").append(
                 $("<th/>").text("이름"),
                 $("<th/>").text("아이디"),
                 $("<th/>").text("학과"),
                 $("<th/>").text("수강과목")
-
             );
             tb.append(row);
 
-
-            //var students = JSON.parse(data);
-            for (var i=0;i<data.length;i++)
-            {
-                var name =data[i].name;
-                var id =data[i].id;
-                var dept= data[i].department;
-                var cls =data[i].class;
-                var sub='';
-                for(var j=0; j<cls.length; j++){
-                    if(j<(cls.length-1)) sub +=cls[j]+',';
-                    else sub+=cls[j];
+            for(var i=0; i<data.length; i++){
+                var name = data[i].name;
+                var id = data[i].id;
+                var dept = data[i].department;
+                var cls = data[i].class;
+                var sub ='';
+                for(var j=0; j<cls.length;j++){
+                    if(j<(cls.length-1)) sub += cls[j]+',';
+                    else sub += cls[j];
                 }
                 var phone = data[i].phone;
-                var row =$("<tr/>").append(
+
+                var row = $("<tr/>").append(
                     $("<td/>").text(name),
                     $("<td/>").text(id),
                     $("<td/>").text(dept),
                     $("<td/>").text(sub)
-                    );
-                tb.append(row);}
-                $("#textbox").html(tb);
-            
+                );
+                tb.append(row);
+            }   
+            $("#textbox").html(tb);
         });
+        
     });
+
+    var req = $.ajax({
+        url:"/rss",
+        dataType: "xml"});
+
+        req.done(function(data){
+            var items = $(data).find("item");
+            if(items.length > 0){
+                items = items.slice(0,5);
+                var uTag = $("<ul/>");
+                items.each(function(){
+                    var item = $(this);
+                    var lk = item.find("link").text();
+                    var title = item.find("title").text();
+                    var aTag = $("<a/>")
+                    .attr({
+                        "href":lk,
+                        "target":"_blank"
+                    })
+                    .text(title);
+                    var liTag = $("<li/>")
+                    .append(aTag);
+                    uTag.append(liTag);
+                });
+                $("#news").html(uTag);
+            }
+        });
+        // req.done(function(data){
+        //  console.log(data);
+        // });
+        req.fail(function (jqXHR, textStatus){
+            alert("failed: " + textStatus);
+        });
+
 });
+//
+//함수 정의는 $(document).ready() 밖에 있어도 문제 없다. 
+function max_open(event){
+    var maxwindow = window.open(event.data.url, "", event.data.winattributes);
+    maxwindow.moveTo(0, 0);
+    maxwindow.resizeTo(screen.availWidth, screen.availHeight);
+}
+
+function flash(){
+    $("#off_test").show().fadeOut("slow");
+}
+
+function update(j){
+    var n = parseInt(j.text(), 10);
+    j.text(n+1);
+}
+
+function show_note_form(){
+    $("#note_form").addClass("popup");
+    change_position($(".popup")); //top 및 left 속성 변경
+    $("#note_form").show("slow").slideDown("slow");
+    $("#note_form").css("display","block");
+    //$("#note_form").show();
+}
+
+function change_position(obj){
+    var l = ($(window).width() - obj.width())/2;
+    var t = ($(window).height() - obj.height())/2;
+    obj.css({top:t, left:l});
+}
+
+function push_note(){
+    var title=$("#note_title").val();
+    var date = $("#note_date").val();
+    var content = $("#note_content").val();
+    var str="<p>" + title + "<br>" + date + "<br>" + content + "</p><br>";
+    $("#note_form").fadeOut("slow");
+    // $("#note_form").css("display","none");
+    //$("#note_form").hide();
+    $("#note").append(str);
+}
